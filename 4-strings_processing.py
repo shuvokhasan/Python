45558601892935b45b3cc21bccd0834c07c6c1e2
Kamrul Hasan Shuvo <57871878+shuvokhasan@users.noreply.github.com>
2023-04-17 17:12:45 +0600

    Add files via upload

---
+++ 4-strings_processing.py
@@ -0,0 +1,8 @@
+name = 'Bangladesh'
+# print(name) # Prints 'name'
+
+# print(name[6]) # [STRING INDEXING] Prints 6th char. from 'name'
+
+print(name[0:6]) # characters from position 0 (included) to 6 (excluded)
+print(name[6:]) # slices till the last index
+print(name[:4]) # slices from the 1st index
---
+++ 5-strings_problem-1.py
@@ -0,0 +1,9 @@
+# Constract 'Python' from the given strings
+
+string1 = 'Happy'
+string2 = 'Merathon'
+
+part1 = string1[3:]
+part2 = string2[4:]
+
+print(part1 + part2)
