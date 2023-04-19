ab2e010b320087a3403b2fa2b8d2ecccd05b5b8a
Kamrul Hasan Shuvo <57871878+shuvokhasan@users.noreply.github.com>
2023-04-19 20:51:25 +0600

    Add files via upload

---
+++ 6-conditionals.py
@@ -0,0 +1,11 @@
+a = 3
+b = 2
+
+if a < b:
+    print("a is smaller")
+elif a == b:
+    print('a is equals to b')
+elif a > b:
+    print("a is greater.")
+else:
+    print('Invalid Input')
---
+++ 7-conditionals_shorthands.py
@@ -0,0 +1,9 @@
+a = 4
+b = 6
+
+print('a is greater') if a > b else print('b is greater')
+
+x = 9
+y = 3
+
+print('x is greater') if x > y else print('y is greater') if x < y else print('x equals y')
