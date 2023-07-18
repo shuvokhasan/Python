c0dd89c06556745136f042bb53365b70d9cee028
Kamrul Hasan Shuvo <57871878+shuvokhasan@users.noreply.github.com>
2023-07-18 16:02:41 +0600

    Add files via upload

---
+++ Coursera/using-web-data-with-python/json2.py
@@ -0,0 +1,20 @@
+import json
+
+# Data as Python list
+input = '''[
+        {"id" : "001",
+        "x" : "2",
+        "name" : "Chuck"
+    },
+        {"id" : "009",
+        "x" : "7",
+        "name" : "Duck"
+    }
+]'''
+
+info = json.loads(input)
+print('User count:', len(info))
+for item in info:
+    print('Name:', item['name'])
+    print('Id:', item['id'])
+    print('Attribute:', item['x'])
